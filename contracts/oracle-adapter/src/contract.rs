ed75d8e00e6027aea64639554e8cd29d400478a4
Gabe Rodriguez <grod220@gmail.com>
2022-12-15 01:08:36 +0100

    alternative

--- contracts/oracle-adapter/src/contract.rs
+++ contracts/oracle-adapter/src/contract.rs
@@ -2,17 +2,15 @@
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     to_binary, Addr, Binary, Decimal, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
-    Uint128,
 };
 use cw2::set_contract_version;
-use cw_controllers_admin_fork::AdminInit::SetInitialAdmin;
 use cw_storage_plus::Bound;
 
+use cw_controllers_admin_fork::AdminInit::SetInitialAdmin;
 use cw_controllers_admin_fork::AdminUpdate;
 use mars_outpost::oracle::PriceResponse;
 use mars_rover::adapters::vault::VaultBase;
 use mars_rover::adapters::Oracle;
-use mars_rover::traits::IntoDecimal;
 
 use crate::error::ContractResult;
 use crate::msg::{
@@ -26,8 +24,6 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 const MAX_LIMIT: u32 = 30;
 const DEFAULT_LIMIT: u32 = 10;
-
-const BASE_AMOUNT: Uint128 = Uint128::new(1_000_000);
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -126,14 +122,14 @@ fn calculate_preview_redeem(
     info: &VaultPricingInfo,
     vault: &VaultBase<Addr>,
 ) -> ContractResult<PriceResponse> {
-    let total_issued = vault.query_total_vault_coins_issued(&deps.querier)?;
-    let price = if total_issued.is_zero() {
+    let vault_coin_supply = vault.query_total_vault_coins_issued(&deps.querier)?;
+    let price = if vault_coin_supply.is_zero() {
         Decimal::zero()
     } else {
-        let amount = vault.query_preview_redeem(&deps.querier, BASE_AMOUNT)?;
+        let total_underlying = vault.query_preview_redeem(&deps.querier, vault_coin_supply)?;
+        let underlying_per_vault_coin = Decimal::from_ratio(total_underlying, vault_coin_supply);
         let price_res = oracle.query_price(&deps.querier, &info.base_denom)?;
-        let value = price_res.price.checked_mul(amount.to_dec()?)?;
-        value.checked_div(BASE_AMOUNT.to_dec()?)?
+        price_res.price.checked_mul(underlying_per_vault_coin)?
     };
 
     Ok(PriceResponse {
