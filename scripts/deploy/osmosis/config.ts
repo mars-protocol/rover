b21c8e98d557140301b55c08a78ae6d851a2be83
brimigs <migliacciobri@gmail.com>
2023-02-15 00:37:04 -0500

    add in testnet multisig owner option

--- scripts/deploy/osmosis/config.ts
+++ scripts/deploy/osmosis/config.ts
@@ -61,7 +61,6 @@ export const osmosisTestnetConfig: DeploymentConfig = {
   // Latest from: https://github.com/mars-protocol/outposts/blob/master/scripts/deploy/addresses/osmo-test-4.json
   oracle: { addr: 'osmo1dqz2u3c8rs5e7w5fnchsr2mpzzsxew69wtdy0aq4jsd76w7upmsstqe0s8' },
   redBank: { addr: 'osmo1t0dl6r27phqetfu0geaxrng0u9zn8qgrdwztapt5xr32adtwptaq6vwg36' },
-  multisigAddr: 'osmo14w4x949nwcrqgfe53pxs3k7x53p0gvlrq34l5n',
   swapRoutes: [
     { denomIn: uosmo, denomOut: uatom, route: [{ token_out_denom: uatom, pool_id: '1' }] },
     { denomIn: uatom, denomOut: uosmo, route: [{ token_out_denom: uosmo, pool_id: '1' }] },
@@ -178,6 +177,7 @@ export const osmosisTestnetConfig: DeploymentConfig = {
 
 export const osmosisMainnetConfig: DeploymentConfig = {
   setMultisigOwner: true,
+  multisigAddr: 'osmo14w4x949nwcrqgfe53pxs3k7x53p0gvlrq34l5n',
   allowedCoins: [
     { denom: uosmo, priceSource: { fixed: { price: '1' } }, grantCreditLine: true },
     {
@@ -288,4 +288,159 @@ export const osmosisMainnetConfig: DeploymentConfig = {
       },
     },
   ],
+}
+
+export const osmosisTestnetMultisigConfig: DeploymentConfig = {
+  setMultisigOwner: true,
+  multisigAddr: 'osmo14w4x949nwcrqgfe53pxs3k7x53p0gvlrq34l5n',
+  allowedCoins: [
+    { denom: uosmo, priceSource: { fixed: { price: '1' } }, grantCreditLine: true },
+    {
+      denom: uatom,
+      priceSource: { arithmetic_twap: { pool_id: 1, window_size: 1800 } },
+      grantCreditLine: true,
+    },
+    {
+      denom: ujuno,
+      priceSource: { arithmetic_twap: { pool_id: 497, window_size: 1800 } },
+      grantCreditLine: true,
+    },
+    {
+      denom: gammPool1,
+      priceSource: { xyk_liquidity_token: { pool_id: 1 } },
+      grantCreditLine: false,
+    },
+    {
+      denom: gammPool497,
+      priceSource: { xyk_liquidity_token: { pool_id: 497 } },
+      grantCreditLine: false,
+    },
+  ],
+  chain: {
+    baseDenom: uosmo,
+    defaultGasPrice: 0.1,
+    id: 'osmo-test-4',
+    prefix: 'osmo',
+    rpcEndpoint: 'https://rpc-test.osmosis.zone',
+  },
+  deployerMnemonic:
+      'rely wonder join knock during sudden slow plate segment state agree also arrest mandate grief ordinary lonely lawsuit hurt super banana rule velvet cart',
+  maxCloseFactor: '0.6',
+  maxUnlockingPositions: '10',
+  maxValueForBurn: '1000000',
+  // Latest from: https://github.com/mars-protocol/outposts/blob/master/scripts/deploy/addresses/osmo-test-4.json
+  oracle: { addr: 'osmo1dqz2u3c8rs5e7w5fnchsr2mpzzsxew69wtdy0aq4jsd76w7upmsstqe0s8' },
+  redBank: { addr: 'osmo1t0dl6r27phqetfu0geaxrng0u9zn8qgrdwztapt5xr32adtwptaq6vwg36' },
+  swapRoutes: [
+    { denomIn: uosmo, denomOut: uatom, route: [{ token_out_denom: uatom, pool_id: '1' }] },
+    { denomIn: uatom, denomOut: uosmo, route: [{ token_out_denom: uosmo, pool_id: '1' }] },
+    { denomIn: uosmo, denomOut: ujuno, route: [{ token_out_denom: ujuno, pool_id: '497' }] },
+    { denomIn: ujuno, denomOut: uosmo, route: [{ token_out_denom: uosmo, pool_id: '497' }] },
+  ],
+  // Latest from: https://stats.apollo.farm/api/vaults/v1/all
+  vaults: [
+    {
+      vault: { address: vaultOsmoAtom1 },
+      config: {
+        deposit_cap: { denom: 'uosmo', amount: '1000000000' }, // 1000 osmo
+        liquidation_threshold: '0.65',
+        max_ltv: '0.63',
+        whitelisted: true,
+      },
+    },
+    {
+      vault: { address: vaultOsmoAtom7 },
+      config: {
+        deposit_cap: { denom: 'uosmo', amount: '1000000000' }, // 1000 osmo
+        liquidation_threshold: '0.65',
+        max_ltv: '0.63',
+        whitelisted: true,
+      },
+    },
+    {
+      vault: { address: vaultOsmoAtom14 },
+      config: {
+        deposit_cap: { denom: 'uosmo', amount: '1000000000' }, // 1000 osmo
+        liquidation_threshold: '0.65',
+        max_ltv: '0.63',
+        whitelisted: true,
+      },
+    },
+    {
+      vault: { address: vaultJunoOsmo1 },
+      config: {
+        deposit_cap: { denom: 'uosmo', amount: '500000000' }, // 500 osmo
+        liquidation_threshold: '0.441',
+        max_ltv: '0.4115',
+        whitelisted: true,
+      },
+    },
+    {
+      vault: { address: vaultJunoOsmo7 },
+      config: {
+        deposit_cap: { denom: 'uosmo', amount: '500000000' }, // 500 osmo
+        liquidation_threshold: '0.441',
+        max_ltv: '0.4115',
+        whitelisted: true,
+      },
+    },
+    {
+      vault: { address: vaultJunoOsmo14 },
+      config: {
+        deposit_cap: { denom: 'uosmo', amount: '500000000' }, // 500 osmo
+        liquidation_threshold: '0.441',
+        max_ltv: '0.4115',
+        whitelisted: true,
+      },
+    },
+  ],
+  testActions: {
+    vault: {
+      depositAmount: '1000000',
+      withdrawAmount: '1000000',
+      mock: {
+        config: {
+          deposit_cap: { denom: uosmo, amount: '100000000' }, // 100 osmo
+          liquidation_threshold: '0.585',
+          max_ltv: '0.569',
+          whitelisted: true,
+        },
+        vaultTokenDenom: udig,
+        type: VaultType.LOCKED,
+        lockup: { time: 900 }, // 15 mins
+        baseToken: gammPool1,
+      },
+    },
+    outpostsDeployerMnemonic:
+        'elevator august inherit simple buddy giggle zone despair marine rich swim danger blur people hundred faint ladder wet toe strong blade utility trial process',
+    borrowAmount: '10',
+    repayAmount: '11',
+    defaultCreditLine: '100000000000',
+    depositAmount: '100',
+    lendAmount: '10',
+    secondaryDenom: uatom,
+    startingAmountForTestUser: '2500000',
+    swap: {
+      slippage: '0.4',
+      amount: '40',
+      route: [
+        {
+          token_out_denom: uatom,
+          pool_id: '1',
+        },
+      ],
+    },
+    unzapAmount: '1000000',
+    withdrawAmount: '12',
+    zap: {
+      coinsIn: [
+        {
+          denom: uatom,
+          amount: '1',
+        },
+        { denom: uosmo, amount: '3' },
+      ],
+      denomOut: gammPool1,
+    },
+  },
 }
---
+++ scripts/deploy/osmosis/testnetMultiIndex.ts
@@ -0,0 +1,10 @@
+import { taskRunner } from '../base'
+import { osmosisTestnetMultisigConfig } from './config'
+
+void (async function () {
+    await taskRunner({
+        config: osmosisTestnetMultisigConfig,
+        swapperContractName: 'mars_swapper_osmosis',
+        zapperContractName: 'mars_zapper_osmosis',
+    })
+})()
