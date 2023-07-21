c4b881480514067eae6325d5015e4350d2adc655
Gabe Rodriguez <grod220@gmail.com>
2023-07-21 14:28:21 +0200

    review updates

--- contracts/credit-manager/src/migrations/v2_0_0.rs
+++ contracts/credit-manager/src/migrations/v2_0_0.rs
@@ -8,6 +8,8 @@ use crate::{
     state::{HEALTH_CONTRACT, INCENTIVES, OWNER, PARAMS, REWARDS_COLLECTOR, SWAPPER},
 };
 
+const FROM_VERSION: &str = "1.0.0";
+
 /// Taken from original Owner package version: https://github.com/mars-protocol/owner/blob/e807c6b12511987577645c8bad68cc7bd6da5398/src/owner.rs#L158
 pub mod v1_owner {
     use cosmwasm_schema::cw_serde;
@@ -55,6 +57,6 @@ pub fn migrate(deps: DepsMut, env: Env, updates: V2Updates) -> ContractResult<Re
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     Ok(Response::new()
         .add_attribute("action", "migrate")
-        .add_attribute("from_version", "1.0.0")
+        .add_attribute("from_version", FROM_VERSION)
         .add_attribute("to_version", CONTRACT_VERSION))
 }
