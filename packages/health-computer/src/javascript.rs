fabb83dccefb0de82db6df34eceff3c5c9e93408
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-07-24 18:17:40 +0200

    compile health-computer for frontend

--- packages/health-computer/src/javascript.rs
+++ packages/health-computer/src/javascript.rs
@@ -1,4 +1,5 @@
 use cosmwasm_schema::serde::{de::DeserializeOwned, Serialize};
+use mars_rover_health_types::BorrowTarget;
 use mars_rover_health_types::HealthValuesResponse;
 use wasm_bindgen::prelude::*;
 
@@ -24,12 +25,12 @@ pub fn max_withdraw_estimate_js(health_computer: JsValue, withdraw_denom: JsValu
 pub fn max_borrow_estimate_js(
     health_computer: JsValue,
     borrow_denom: JsValue,
-    target: BorrowTarget,
+    target: JsValue,
 ) -> JsValue {
     let c: HealthComputer = deserialize(health_computer);
     let denom: String = deserialize(borrow_denom);
     let target: BorrowTarget = deserialize(target);
-    let max = c.max_borrow_amount_estimate(&denom, &target).unwrap();
+    let max = c.max_borrow_amount_estimate(&denom, target).unwrap();
     serialize(max)
 }
 
