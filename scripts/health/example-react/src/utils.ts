9469f623a43b4dc51330ccb7fe6345a417de537e
Bob van der Helm <34470358+bobthebuidlr@users.noreply.github.com>
2023-09-14 09:57:57 +0200

    update contracts and rpc for devnet

--- scripts/health/example-react/src/utils.ts
+++ scripts/health/example-react/src/utils.ts
@@ -1,36 +1,31 @@
-import { Positions } from '../../../types/generated/mars-credit-manager/MarsCreditManager.types'
+import {Positions} from '../../../types/generated/mars-credit-manager/MarsCreditManager.types'
 
-import init, {
-  compute_health_js,
-  max_withdraw_estimate_js,
-  max_borrow_estimate_js,
-} from '../../pkg-web'
-import { HealthValuesResponse } from '../../../types/generated/mars-rover-health-types/MarsRoverHealthTypes.types'
-import { DataFetcher } from '../../DataFetcher'
-import { osmosisTestnetConfig } from '../../../deploy/osmosis/testnet-config'
+import init, {compute_health_js, max_borrow_estimate_js, max_withdraw_estimate_js,} from '../../pkg-web'
+import {HealthValuesResponse} from '../../../types/generated/mars-rover-health-types/MarsRoverHealthTypes.types'
+import {DataFetcher} from '../../DataFetcher'
 
 const getFetcher = (cmAddress: string) => {
-  return new DataFetcher(
-    compute_health_js,
-    max_withdraw_estimate_js,
-    max_borrow_estimate_js,
-    cmAddress,
-    osmosisTestnetConfig.oracle.addr,
-    osmosisTestnetConfig.redBank.addr,
-    osmosisTestnetConfig.chain.rpcEndpoint,
-  )
+    return new DataFetcher(
+        compute_health_js,
+        max_withdraw_estimate_js,
+        max_borrow_estimate_js,
+        'osmo1m83kw2vehyt9urxf79qa9rxk8chgs4464e5h8s37yhnw3pwauuqq7lux8r',
+        'osmo156elt2tp5455q9a6vfrvnpncxyd33cxm9z2lgguwg6dgws9tedps5tq3rc',
+        'osmo1vxpdcw092n9rngvekve8g324c2yjx56496ck98ag4sdxr4p4zd4q0wr7x6',
+        'https://rpc.devnet.osmosis.zone/',
+    )
 }
 
 export const fetchPositions = async (cmAddress: string, accountId: string): Promise<Positions> => {
-  const dataFetcher = getFetcher(cmAddress)
-  return await dataFetcher.fetchPositions(accountId)
+    const dataFetcher = getFetcher(cmAddress)
+    return await dataFetcher.fetchPositions(accountId)
 }
 
 export const fetchHealth = async (
-  cmAddress: string,
-  accountId: string,
+    cmAddress: string,
+    accountId: string,
 ): Promise<HealthValuesResponse> => {
-  await init()
-  const dataFetcher = getFetcher(cmAddress)
-  return await dataFetcher.computeHealth(accountId)
+    await init()
+    const dataFetcher = getFetcher(cmAddress)
+    return await dataFetcher.computeHealth(accountId)
 }
